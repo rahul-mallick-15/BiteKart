c4b5f34a40dd9335835cf01b522e40f9e8fa6ae1
Rahul Mallick <rahulmallick1115@gmail.com>
2024-06-08 01:56:57 +0530

    Fixed logical errors

--- controllers/contacts.ts
+++ controllers/contacts.ts
@@ -16,7 +16,7 @@ export const createContact = asyncWrapper(async (req: any, res: any, next: any) 
     if(!email && !phoneNumber){
         return res.status(400).json({ success: false, msg: "Please provide email or phone number" })
     }
-    let [results, metadata] = await sequelize.query('SELECT id, "linkPrecedence" FROM "Contact" WHERE ("email" = ? or "phoneNumber" = ?) and "linkPrecedence"=\'primary\'', {replacements: [email, phoneNumber]});
+    let [results, metadata] = await sequelize.query('SELECT id, "linkedId", "linkPrecedence" FROM "Contact" WHERE ("email" = ? or "phoneNumber" = ?) order by "linkPrecedence"', {replacements: [email, phoneNumber]});
     let createdContact;
     if(results.length == 0){
         createdContact = await Contact.create({
@@ -26,15 +26,15 @@ export const createContact = asyncWrapper(async (req: any, res: any, next: any) 
         });
     }
     else{
-        const [{id}] = results as {id:number}[];
+        const {id, linkedId, linkPrecedence} = results[0] as {id:number,linkedId:number,linkPrecedence:string}
         createdContact = await Contact.create({
             email: email,
             phoneNumber: phoneNumber,
             linkPrecedence: "secondary",
-            linkedId: id
+            linkedId: (linkPrecedence == 'primary' ? id : linkedId)
         });
     }
-    console.log(results);
+    // console.log(results);
     return res.status(201).json({ success: true, createdContact })
 })
 
@@ -42,7 +42,7 @@ export const identifyContact = asyncWrapper(async (req: any, res: any, next: any
     const { email, phoneNumber } = req.body;
     console.log(email,phoneNumber);
     
-    let [results, metadata] = await sequelize.query('SELECT id, email, "phoneNumber", "linkPrecedence", "linkedId" FROM "Contact" WHERE email = ? or "phoneNumber" = ? order by "linkPrecedence", "createdAt"', { replacements: [email, phoneNumber] });
+    let [results, metadata] = await sequelize.query('SELECT id, email, "phoneNumber", "linkPrecedence", "linkedId" FROM "Contact" WHERE email = ? or "phoneNumber" = ? order by "linkPrecedence", "createdAt" desc', { replacements: [email, phoneNumber] });
     if(results.length == 0){
         return createContact(req,res,next);
     }
@@ -55,23 +55,34 @@ export const identifyContact = asyncWrapper(async (req: any, res: any, next: any
         if(!emailExists || !phoneNumberExists){
             return createContact(req, res, next);
         }
-    }
 
-    // Count matches with linkPrecedence as "primary"
-    const primaryCount = results.filter((contact: any) => contact.linkPrecedence === "primary").length;
-    if(primaryCount == 2){
-        const {id:linkedId} = results[0] as {id:number};
-        const {id} = results[1] as {id:number};
-        await Contact.update(
-            {
-              linkPrecedence: 'secondary',
-              linkedId,
-              updatedAt: new Date(), // Set updatedAt to the current timestamp
-            },
-            {
-              where: { [Op.or]:[{id: id}, {linkedId: id}] },
-            }
-          );
+        let ids = new Set<number>();
+        for(let i = 0; i < results.length; i++){
+            const {id, linkedId, linkPrecedence} = results[i] as {id:number,linkedId:number,linkPrecedence:string};
+            ids.add(linkPrecedence == "primary" ? id : linkedId);
+        }
+
+        if (ids.size > 1) {
+            [results, metadata] = await sequelize.query(
+              `SELECT id FROM "Contact" WHERE id IN (:ids) ORDER BY "createdAt" desc`,
+              {
+                replacements: { ids: Array.from(ids) },
+              }
+            );
+            const id = (results[0] as {id:number}).id ;
+            await Contact.update(
+                {
+                  linkPrecedence: 'secondary',
+                  updatedAt: new Date(), // Set updatedAt to the current timestamp
+                  linkedId: (results[1] as {id:number}).id
+                },
+                {
+                  where: { [Op.or]:[{id: id}, {linkedId: id}] },
+                }
+              );
+
+            [results, metadata] = await sequelize.query('SELECT id, email, "phoneNumber", "linkPrecedence", "linkedId" FROM "Contact" WHERE email = ? or "phoneNumber" = ? order by "linkPrecedence"', { replacements: [email, phoneNumber] });
+        }   
     }
 
     const {linkPrecedence} = results[0] as {linkPrecedence:string}
@@ -88,6 +99,6 @@ export const identifyContact = asyncWrapper(async (req: any, res: any, next: any
     const emails: string[] = Array.from(new Set((results as { email: string }[]).map((item) => item.email)));
     const phoneNumbers: string[] = Array.from(new Set((results as { phoneNumber: string }[]).map((item) => item.phoneNumber)));
     const secondaryContactIds = (results as { id: number }[]).slice(1).map(obj => obj.id);
-    console.log(results);
+    // console.log(results);
     res.status(200).json({ contact: {primaryContactId,emails,phoneNumbers,secondaryContactIds}})
 })
